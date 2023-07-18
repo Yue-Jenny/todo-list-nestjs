4eb3a2e58314a769d16e21db223f65d449bcc476
jennychiang <jennyc201911@gmail.com>
2023-07-18 18:45:02 +0800

    add: rename folder name from entities to schema

--- src/schema/todos.ts
+++ src/schema/todos.ts
@@ -1,5 +1,11 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  DeleteDateColumn,
+} from 'typeorm';
 
+// 建立 db schema 用的
 @Entity({ name: 'Todos' })
 export class Todos {
   @PrimaryGeneratedColumn('increment') // PrimaryGeneratedColumn 的主要功能是生成一個自增或者UUID（通用唯一標識符）類型的主鍵欄位。
@@ -10,4 +16,7 @@ export class Todos {
 
   @Column({ default: false, type: 'boolean' })
   completed: boolean;
+
+  @DeleteDateColumn()
+  deletedDate: Date;
 }
